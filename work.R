8ffc778589195206be2e7c39e40a36f644fe892e
Oliver Kliegel <oliver.kliegel@gmail.com>
2014-08-16 21:54:32 +0200

    Assignment finished

---
+++ work.R
@@ -0,0 +1,9 @@
+library("knitr")
+library("rmarkdown")
+setwd("Z:/Dokumente/Coursera/Reproducible Research/Assignment1/RepData_PeerAssessment1")
+
+studis <- read.csv("activity.csv", header=T)
+
+
+
+head(studis,50)
