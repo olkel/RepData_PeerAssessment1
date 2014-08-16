8d6f5227b1113bc3bbebfe34982ada0c1217d25a
Oliver Kliegel <oliver.kliegel@gmail.com>
2014-08-16 21:56:20 +0200

    Revert "Assignment finished"
    This reverts commit 8ffc778589195206be2e7c39e40a36f644fe892e.

--- work.R
+++
@@ -1,9 +0,0 @@
-library("knitr")
-library("rmarkdown")
-setwd("Z:/Dokumente/Coursera/Reproducible Research/Assignment1/RepData_PeerAssessment1")
-
-studis <- read.csv("activity.csv", header=T)
-
-
-
-head(studis,50)
